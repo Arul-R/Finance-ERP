4d39a3640bfa3ad1455412171739ae908342dc0d
Arul-R <arulrajeev1@gmail.com>
2025-05-07 18:10:37 +0530

    updated at work

--- backend/employee-services/src/models/Employee.js
+++ backend/employee-services/src/models/Employee.js
@@ -6,7 +6,12 @@ const employeeSchema = new mongoose.Schema({
   role:        { type: String, required: true },
   base_salary: { type: Number, required: true },
   salary_type: { type: String, enum: ['monthly', 'hourly'], required: true },
-  active:      { type: Boolean, default: true },
-}, { timestamps: true });
+  active:      { type: Boolean, default: true }, createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date }  
+});
+employeeSchema.pre('findOneAndUpdate', function (next) {
+  this.set({ updatedAt: new Date() });
+  next();
+});
 
 module.exports = mongoose.model('Employee', employeeSchema);
